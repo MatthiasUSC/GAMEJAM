cce4687c54e99a07ce76b2f85fcb6da190903387
MiniMinja <minyoungheo.heo@gmail.com>
2023-11-05 14:58:03 -0800

    Finished title screen. min

---
+++ Assets/Min/TitleScreenScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenScript : MonoBehaviour
+{
+    GameObject fadetoblack_obj;
+
+    void Start()
+    {
+        fadetoblack_obj = GameObject.FindWithTag("FadeToBlack");
+        fadetoblack_obj.SetActive(false);
+    }
+
+    public void StartGame(){
+        fadetoblack_obj.SetActive(true);
+        StartCoroutine(FadeToBlackPlayAndWait());
+    }
+
+    public void ShowCredits(){
+
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    IEnumerator FadeToBlackPlayAndWait(){
+        Animator fadetoblackanimation = fadetoblack_obj.GetComponent<Animator>();
+        fadetoblackanimation.SetTrigger("FadeToBlack");
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Sample Scene");
+    }
+}
